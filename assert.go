bac846a81ebf00fecf45ec5b3353901ead40bb34
chai2010 <chaishushan@gmail.com>
2014-09-19 15:04:25 +0800

    more help funcs

--- assert.go
+++ assert.go
@@ -20,16 +20,100 @@ Example:
 		Assert(t, 1 == 1)
 	}
 
-	func TestAssertEQ(t *testing.T) {
-		AssertEQ(t, 2, 1+1)
+	func TestAssertTrue(t *testing.T) {
+		AssertTrue(t, true)
+	}
+
+	func TestAssertFalse(t *testing.T) {
+		AssertFalse(t, false)
+	}
+
+	func TestAssertEqual(t *testing.T) {
+		AssertEqual(t, 2, 1+1)
+	}
+
+	func TestAssertNotEqual(t *testing.T) {
+		AssertNotEqual(t, 2, 1)
 	}
 
 	func TestAssertNear(t *testing.T) {
 		AssertNear(t, math.Sqrt(2), 1.414, 0.1)
 	}
 
+	func TestAssertBetween(t *testing.T) {
+		AssertBetween(t, 0, 255, 0)
+		AssertBetween(t, 0, 255, 128)
+		AssertBetween(t, 0, 255, 255)
+	}
+
+	func TestAssertNotBetween(t *testing.T) {
+		AssertNotBetween(t, 0, 255, -1)
+		AssertNotBetween(t, 0, 255, 256)
+	}
+
 	func TestAssertMatch(t *testing.T) {
 		AssertMatch(t, `^\w+@\w+\.com$`, "chaishushan@gmail.com")
+	}
+
+	func TestAssertSliceContain(t *testing.T) {
+		AssertSliceContain(t, []int{1, 1, 2, 3, 5, 8, 13}, 8)
+	}
+
+	func TestAssertSliceNotContain(t *testing.T) {
+		AssertSliceNotContain(t, []int{1, 1, 2, 3, 5, 8, 13}, 12)
+	}
+
+	func TestAssertMapContain(t *testing.T) {
+		AssertMapContain(t,
+			map[string]int{
+				"UTC": 0 * 60 * 60,
+				"EST": -5 * 60 * 60,
+				"CST": -6 * 60 * 60,
+				"MST": -7 * 60 * 60,
+				"PST": -8 * 60 * 60,
+			},
+			"MST", -7*60*60,
+		)
+
+	}
+
+	func TestAssertMapNotContain(t *testing.T) {
+		AssertMapNotContain(t,
+			map[string]int{
+				"UTC": 0 * 60 * 60,
+				"EST": -5 * 60 * 60,
+				"CST": -6 * 60 * 60,
+				"MST": -7 * 60 * 60,
+				"PST": -8 * 60 * 60,
+			},
+			"ABC", -7*60*60,
+		)
+	}
+
+	func TestAssertZero(t *testing.T) {
+		AssertZero(t, struct {
+			A bool
+			B string
+			C int
+			d map[string]interface{}
+		}{})
+	}
+
+	func TestAssertNotZero(t *testing.T) {
+		AssertNotZero(t, struct {
+			A bool
+			B string
+			C int
+			d map[string]interface{}
+		}{A: true})
+	}
+
+	func TestAssertFileExists(t *testing.T) {
+		AssertFileExists(t, "assert.go")
+	}
+
+	func TestAssertFileNotExists(t *testing.T) {
+		AssertFileNotExists(t, "assert.cc")
 	}
 
 Report bugs to <chaishushan@gmail.com>.
@@ -41,6 +125,7 @@ package assert
 import (
 	"fmt"
 	"math"
+	"os"
 	"reflect"
 	"regexp"
 	"testing"
@@ -56,12 +141,42 @@ func Assert(t testing.TB, condition bool, args ...interface{}) {
 	}
 }
 
-func AssertEQ(t testing.TB, expected, got interface{}, args ...interface{}) {
+func AssertTrue(t testing.TB, condition bool, args ...interface{}) {
+	if condition != true {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertTrue failed, %s", msg)
+		} else {
+			t.Fatal("AssertTrue failed")
+		}
+	}
+}
+
+func AssertFalse(t testing.TB, condition bool, args ...interface{}) {
+	if condition != false {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertFalse failed, %s", msg)
+		} else {
+			t.Fatal("AssertFalse failed")
+		}
+	}
+}
+
+func AssertEqual(t testing.TB, expected, got interface{}, args ...interface{}) {
 	if !reflect.DeepEqual(expected, got) {
 		if msg := fmt.Sprint(args...); msg != "" {
-			t.Fatalf("AssertEQ failed, expected = %v, got = %v, %s", expected, got, msg)
+			t.Fatalf("AssertEqual failed, expected = %v, got = %v, %s", expected, got, msg)
 		} else {
-			t.Fatalf("AssertEQ failed, expected = %v, got = %v", expected, got)
+			t.Fatalf("AssertEqual failed, expected = %v, got = %v", expected, got)
+		}
+	}
+}
+
+func AssertNotEqual(t testing.TB, expected, got interface{}, args ...interface{}) {
+	if reflect.DeepEqual(expected, got) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertNotEqual failed, expected = %v, got = %v, %s", expected, got, msg)
+		} else {
+			t.Fatalf("AssertNotEqual failed, expected = %v, got = %v", expected, got)
 		}
 	}
 }
@@ -72,6 +187,26 @@ func AssertNear(t testing.TB, expected, got, abs float64, args ...interface{}) {
 			t.Fatalf("AssertNear failed, expected = %v, got = %v, abs = %v, %s", expected, got, abs, msg)
 		} else {
 			t.Fatalf("AssertNear failed, expected = %v, got = %v, abs = %v", expected, got, abs)
+		}
+	}
+}
+
+func AssertBetween(t testing.TB, min, max, val float64, args ...interface{}) {
+	if val < min || max < val {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertBetween failed, min = %v, max = %v, val = %v, %s", min, max, val, msg)
+		} else {
+			t.Fatalf("AssertBetween failed, min = %v, max = %v, val = %v", min, max, val)
+		}
+	}
+}
+
+func AssertNotBetween(t testing.TB, min, max, val float64, args ...interface{}) {
+	if min <= val && val <= max {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertNotBetween failed, min = %v, max = %v, val = %v, %s", min, max, val, msg)
+		} else {
+			t.Fatalf("AssertNotBetween failed, min = %v, max = %v, val = %v", min, max, val)
 		}
 	}
 }
@@ -90,6 +225,118 @@ func AssertMatch(t *testing.T, expectedPattern, got string, args ...interface{})
 			} else {
 				t.Fatalf("AssertMatch failed, expected = %q, got = %v", expectedPattern, got)
 			}
+		}
+	}
+}
+
+func AssertSliceContain(t *testing.T, slice, elem interface{}, args ...interface{}) {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() != reflect.Slice {
+		panic(fmt.Sprintf("AssertSliceContain called with non-slice value of type %T", slice))
+	}
+	var contained bool
+	for i := 0; i < sliceVal.Len(); i++ {
+		if reflect.DeepEqual(sliceVal.Index(i).Interface(), elem) {
+			contained = true
+			break
+		}
+	}
+	if !contained {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertSliceContain failed, slice = %v, elem = %v, %s", slice, elem, msg)
+		} else {
+			t.Fatalf("AssertSliceContain failed, slice = %v, elem = %v", slice, elem)
+		}
+	}
+}
+
+func AssertSliceNotContain(t *testing.T, slice, elem interface{}, args ...interface{}) {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() != reflect.Slice {
+		panic(fmt.Sprintf("AssertSliceContain called with non-slice value of type %T", slice))
+	}
+	var contained bool
+	for i := 0; i < sliceVal.Len(); i++ {
+		if reflect.DeepEqual(sliceVal.Index(i).Interface(), elem) {
+			contained = true
+			break
+		}
+	}
+	if contained {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertSliceNotContain failed, slice = %v, elem = %v, %s", slice, elem, msg)
+		} else {
+			t.Fatalf("AssertSliceNotContain failed, slice = %v, elem = %v", slice, elem)
+		}
+	}
+}
+
+func AssertMapContain(t *testing.T, m, key, elem interface{}, args ...interface{}) {
+	mapVal := reflect.ValueOf(m)
+	if mapVal.Kind() != reflect.Map {
+		panic(fmt.Sprintf("AssertMapContain called with non-map value of type %T", m))
+	}
+	elemVal := mapVal.MapIndex(reflect.ValueOf(key))
+	if !elemVal.IsValid() && !reflect.DeepEqual(elemVal.Interface(), elem) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertMapContain failed, map = %v, key = %v, elem = %v, %s", m, key, elem, msg)
+		} else {
+			t.Fatalf("AssertMapContain failed, map = %v, key = %v, elem = %v", m, key, elem)
+		}
+	}
+}
+
+func AssertMapNotContain(t *testing.T, m, key, elem interface{}, args ...interface{}) {
+	mapVal := reflect.ValueOf(m)
+	if mapVal.Kind() != reflect.Map {
+		panic(fmt.Sprintf("AssertMapContain called with non-map value of type %T", m))
+	}
+	elemVal := mapVal.MapIndex(reflect.ValueOf(key))
+	if elemVal.IsValid() && reflect.DeepEqual(elemVal.Interface(), elem) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertMapContain failed, map = %v, key = %v, elem = %v, %s", m, key, elem, msg)
+		} else {
+			t.Fatalf("AssertMapContain failed, map = %v, key = %v, elem = %v", m, key, elem)
+		}
+	}
+}
+
+func AssertZero(t *testing.T, val interface{}, args ...interface{}) {
+	if !reflect.DeepEqual(reflect.Zero(reflect.TypeOf(val)).Interface(), val) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertZero failed, val = %v, %s", val, msg)
+		} else {
+			t.Fatalf("AssertZero failed, val = %v", val)
+		}
+	}
+}
+
+func AssertNotZero(t *testing.T, val interface{}, args ...interface{}) {
+	if reflect.DeepEqual(reflect.Zero(reflect.TypeOf(val)).Interface(), val) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertNotZero failed, val = %v, %s", val, msg)
+		} else {
+			t.Fatalf("AssertNotZero failed, val = %v", val)
+		}
+	}
+}
+
+func AssertFileExists(t *testing.T, path string, args ...interface{}) {
+	if _, err := os.Stat(path); err != nil {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertFileExists failed, path = %v, err = %v, %s", path, err, msg)
+		} else {
+			t.Fatalf("AssertFileExists failed, path = %v, err = %v", path, err)
+		}
+	}
+}
+
+func AssertFileNotExists(t *testing.T, path string, args ...interface{}) {
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		if msg := fmt.Sprint(args...); msg != "" {
+			t.Fatalf("AssertFileNotExists failed, path = %v, err = %v, %s", path, err, msg)
+		} else {
+			t.Fatalf("AssertFileNotExists failed, path = %v, err = %v", path, err)
 		}
 	}
 }
--- example_test.go
+++ example_test.go
@@ -15,14 +15,98 @@ func TestAssert(t *testing.T) {
 	Assert(t, 1 == 1)
 }
 
-func TestAssertEQ(t *testing.T) {
-	AssertEQ(t, 2, 1+1)
+func TestAssertTrue(t *testing.T) {
+	AssertTrue(t, true)
+}
+
+func TestAssertFalse(t *testing.T) {
+	AssertFalse(t, false)
+}
+
+func TestAssertEqual(t *testing.T) {
+	AssertEqual(t, 2, 1+1)
+}
+
+func TestAssertNotEqual(t *testing.T) {
+	AssertNotEqual(t, 2, 1)
 }
 
 func TestAssertNear(t *testing.T) {
 	AssertNear(t, math.Sqrt(2), 1.414, 0.1)
 }
 
+func TestAssertBetween(t *testing.T) {
+	AssertBetween(t, 0, 255, 0)
+	AssertBetween(t, 0, 255, 128)
+	AssertBetween(t, 0, 255, 255)
+}
+
+func TestAssertNotBetween(t *testing.T) {
+	AssertNotBetween(t, 0, 255, -1)
+	AssertNotBetween(t, 0, 255, 256)
+}
+
 func TestAssertMatch(t *testing.T) {
 	AssertMatch(t, `^\w+@\w+\.com$`, "chaishushan@gmail.com")
+}
+
+func TestAssertSliceContain(t *testing.T) {
+	AssertSliceContain(t, []int{1, 1, 2, 3, 5, 8, 13}, 8)
+}
+
+func TestAssertSliceNotContain(t *testing.T) {
+	AssertSliceNotContain(t, []int{1, 1, 2, 3, 5, 8, 13}, 12)
+}
+
+func TestAssertMapContain(t *testing.T) {
+	AssertMapContain(t,
+		map[string]int{
+			"UTC": 0 * 60 * 60,
+			"EST": -5 * 60 * 60,
+			"CST": -6 * 60 * 60,
+			"MST": -7 * 60 * 60,
+			"PST": -8 * 60 * 60,
+		},
+		"MST", -7*60*60,
+	)
+
+}
+
+func TestAssertMapNotContain(t *testing.T) {
+	AssertMapNotContain(t,
+		map[string]int{
+			"UTC": 0 * 60 * 60,
+			"EST": -5 * 60 * 60,
+			"CST": -6 * 60 * 60,
+			"MST": -7 * 60 * 60,
+			"PST": -8 * 60 * 60,
+		},
+		"ABC", -7*60*60,
+	)
+}
+
+func TestAssertZero(t *testing.T) {
+	AssertZero(t, struct {
+		A bool
+		B string
+		C int
+		d map[string]interface{}
+	}{})
+}
+
+func TestAssertNotZero(t *testing.T) {
+	AssertNotZero(t, struct {
+		A bool
+		B string
+		C int
+		d map[string]interface{}
+	}{A: true})
+}
+
+func TestAssertFileExists(t *testing.T) {
+	AssertFileExists(t, "assert.go")
+}
+
+func TestAssertFileNotExists(t *testing.T) {
+	AssertFileNotExists(t, "assert.cc")
 }
